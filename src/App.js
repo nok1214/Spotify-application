d4e9cad8b8c8a5c533778999dc291d3a8fb12c35
nok1214 <80727744+nok1214@users.noreply.github.com>
2021-07-30 09:51:04 -0700

    modified code to better fit Client Credentials Flow redirect process

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from "react";
 import Login from "./components/Login";
-import { getTokenFromResponse } from "./spotify";
 import Dashboard from "./components/Dashboard";
+//import { getTokenFromResponse } from "./spotify";
 
 export default function App() {
   const redirect = () => {
-    const hash = getTokenFromResponse();
-    window.location.hash = "";
-    let _token = hash.access_token;
-
-    if (!_token) {
-      return <Login />;
-    } else {
+    const accessTokenMatch = window.location.hash.match(/access_token=([^&]*)/);
+    if (accessTokenMatch) {
+      let _token = accessTokenMatch[1];
+      console.log(accessTokenMatch[1]);
       return <Dashboard token={_token} />;
+    } else {
+      return <Login />;
     }
+
+    // ====for Implicit Grant use ====
+
+    // if (!_token) {
+    //   return <Login />;
+    // } else {
+    //   return <Dashboard token={_token} />;
+    // }
+
+    // ======      end      ======
   };
 
   return <div>{redirect()}</div>;
